17ca9caaf5d64b5fe9d340fb320fb9828c125cc7
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-03-14 14:57:42 +0545

    add demo section

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,14 @@
 import HeroSection from "@/components/home/hero-section";
+import DemoSection from "@/components/home/demo-section";
+import BgGradient from "@/components/common/bg-gradient";
 export default function Home() {
   return (
     <div className="relative w-full">
-      <HeroSection />
-      {/* <DemoSection /> */}
+      <BgGradient />
+      <div className="flex flex-col">
+        <HeroSection />
+        <DemoSection />
+      </div>
       {/* <HowItWorksSection /> */}
       {/* <PricingSection /> */}
       {/* <CTASection /> */}
