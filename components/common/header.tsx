92f034c6547acf5d3ff5994446a57e1ba40d03a7
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-03-14 17:52:07 +0545

    auth with clerk setup

--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,9 +1,9 @@
 import NavLink from "./nav-link";
 import { FileText } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
 
 export default function Header() {
-  const isLoggedIn = false;
   return (
     <nav
       className="container flex items-center 
@@ -24,20 +24,23 @@ export default function Header() {
 
       <div className="flex lg:justify-center gap-4 lg:gap-12 lg:items-center">
         <NavLink href="/#pricing">Pricing</NavLink>
-        {isLoggedIn && <NavLink href="/dashboard">Your Summaries</NavLink>}
+        <SignedIn>
+          <NavLink href="/dashboard">Your Summaries</NavLink>
+        </SignedIn>
       </div>
       <div className="flex lg:justify-end lg:flex-1">
-        {isLoggedIn ? (
+        <SignedIn>
           <div className="flex gap-2 items-center">
             <NavLink href="/upload">Upload a PDF</NavLink>
             <div>Pro</div>
-            <Button>User</Button>
+            <SignedIn>
+              <UserButton />
+            </SignedIn>
           </div>
-        ) : (
-          <div>
-            <NavLink href="/sign-in">Sign In</NavLink>
-          </div>
-        )}
+        </SignedIn>
+        <SignedOut>
+          <NavLink href="/sign-in">Sign In</NavLink>
+        </SignedOut>
       </div>
     </nav>
   );
