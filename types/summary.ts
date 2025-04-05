6a9aed174b89c5d46f494665a4fc7c279f195e91
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-04-05 17:11:33 +0545

    interfaces for summary

---
+++ types/summary.ts
@@ -0,0 +1,11 @@
+export interface Summary {
+  id: string;
+  user_id: string;
+  original_file_url: string;
+  summary_text: string;
+  status: string;
+  title: string;
+  file_name: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
