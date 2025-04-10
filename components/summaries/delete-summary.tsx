25f375a02924435d7a0d746146641648be1ca61a
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-04-10 13:43:23 +0545

    all good

--- components/summaries/delete-summary.tsx
+++ components/summaries/delete-summary.tsx
@@ -41,7 +41,7 @@ export default function DeleteSummary({ summaryId }: DeleteSummaryInterface) {
           variant={"ghost"}
           size="icon"
           className="text-gray-400 bg-gray-50 border border-gray-200
-       hover:text-rose-600 hover:bg-rose-50"
+         hover:text-rose-600 hover:bg-rose-50"
         >
           <Trash2 className="w-4 h-4" />
         </Button>
