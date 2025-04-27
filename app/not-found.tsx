70ee96e45cdca6f1a004e9fae8d49a957975f5da
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-04-27 21:31:02 +0545

    not use loader here

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,12 +1,7 @@
-import { Suspense } from "react";
-import Loading from "@/components/common/loading";
 export default function NotFound() {
   return (
     <div className="h-screen w-screen flex items-center justify-center bg-white">
       <h1 className="text-4xl font-bold text-gray-800">404 - Page Not Found</h1>
-      <Suspense fallback={null}>
-        <Loading />
-      </Suspense>
     </div>
   );
 }
