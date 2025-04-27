f3230e609e921710cbc16619e9274b390de6c689
Sumiran Dahal <sumdahl@Sumirans-MacBook-Air.local>
2025-04-27 21:31:44 +0545

    remove suspense and loader component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,11 @@
 import type { Metadata } from "next";
 import { Source_Sans_3 as FontSans } from "next/font/google";
 import "./globals.css";
-
 import { ClerkProvider } from "@clerk/nextjs";
 import Header from "@/components/common/header";
 import Footer from "@/components/common/footer";
 
 import { Toaster } from "sonner";
-import Loading from "@/components/common/loading";
 
 const fontSans = FontSans({
   variable: "--font-sans",
@@ -35,9 +33,7 @@ export default function RootLayout({
             <main className="flex-1">{children}</main>
             <Footer />
           </div>
-
           <Toaster position="top-right" richColors expand duration={3000} />
-          <Loading />
         </body>
       </html>
     </ClerkProvider>
